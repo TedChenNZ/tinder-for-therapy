14fc27636e8907a43334607c732ac274f6824090
Ted Chen <tedchennz@gmail.com>
2021-11-21 16:22:57 +1300

    Hide react query dev tools

--- src/pages/App/setup/SetupProviders.tsx
+++ src/pages/App/setup/SetupProviders.tsx
@@ -3,12 +3,14 @@ import { QueryClient, QueryClientProvider } from "react-query";
 import { StyleProviders } from "./StyleProviders";
 import { ReactQueryDevtools } from "react-query/devtools";
 
+const SHOW_DEV_TOOLS = false;
+
 export const SetupProviders: React.FC = ({ children }) => {
 	const [queryClient] = useState(new QueryClient());
 	return (
 		<QueryClientProvider client={queryClient}>
 			<StyleProviders>{children}</StyleProviders>
-			<ReactQueryDevtools />
+			{SHOW_DEV_TOOLS && <ReactQueryDevtools />}
 		</QueryClientProvider>
 	);
 };
